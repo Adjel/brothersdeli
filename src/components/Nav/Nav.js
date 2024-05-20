ac41ed5b674a3a6c1455f27aa62b23128a883b9b
Adjel <adjel.touaoula@gmail.com>
2024-05-20 20:50:31 +0200

    first commit debugging

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import styles from "./nav.module.css";
 
 function Nav() {
   return (
-    <>
-      <Link href="notre-histoire">Notre histoire</Link>
+    <nav>
+      <div className={styles.navLink}>TEST</div>
+      <Link className={styles.navLink} href="notre-histoire">
+        Notre histoire
+      </Link>
       <Link href="la-carte">La carte</Link>
       <Link href="devenir-franchise">devenir franchisé</Link>
       <Link href="contact">Contact</Link>
-    </>
+    </nav>
   );
 }
 
