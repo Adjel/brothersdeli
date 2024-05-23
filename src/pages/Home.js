30996b2e218c75e18a5350a5a6eebc1668306c11
Adjel <adjel.touaoula@gmail.com>
2024-05-23 14:42:30 +0200

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
 "use client";
 import React from "react";
+import styles from "../styles/home.module.css";
+import Link from "next/link";
+import OrderingButton from "@/components/OrderingButton";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className={styles.section}>
+      <Link className={styles.link} href="/">
+        <h1 className={styles.h1}>BROTHERS DELI, LA RÉFÉRENCE DU PASTRAMI</h1>
+      </Link>
+      <OrderingButton />
+    </section>
+  );
 }
