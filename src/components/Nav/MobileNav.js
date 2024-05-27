f272ac696297a4aaed84ded6fa451fbae0f7c20b
Adjel <adjel.touaoula@gmail.com>
2024-05-27 16:10:41 +0200

    feat(nav): current link color change on nav

--- src/components/Nav/MobileNav.js
+++ src/components/Nav/MobileNav.js
@@ -1,9 +1,17 @@
 import React, { useRef, useEffect } from "react";
 import Link from "next/link";
 import styles from "./nav.module.css";
+import { useRouter } from "next/router";
 
 export default function MobileNav({ isNavOpen }) {
   const navRef = useRef();
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  const notreHistoire = "/Brand";
+  const laCarte = "/Menu";
+  const franchise = "/BecomeFranchise";
+  const contact = "/Contact";
 
   useEffect(() => {
     if (isNavOpen) {
@@ -19,23 +27,47 @@ export default function MobileNav({ isNavOpen }) {
       className={`${styles.wrapper} ${isNavOpen ? styles.open : styles.closed}`}
     >
       <ul className={styles.mobileNavWrapper}>
-        <il className={styles.liLinkWrapper}>
-          <Link className={styles.navLink} href="notre-histoire">
+        <il className={styles.itemLinkWrapper}>
+          <Link
+            className={
+              currentPath !== notreHistoire
+                ? styles.navLink
+                : styles.navLinkSelected
+            }
+            href="notre-histoire"
+          >
             Notre histoire
           </Link>
         </il>
-        <il className={styles.liLinkWrapper}>
-          <Link className={styles.navLink} href="la-carte">
+        <il className={styles.itemLinkWrapper}>
+          <Link
+            className={
+              currentPath !== laCarte ? styles.navLink : styles.navLinkSelected
+            }
+            href="la-carte"
+          >
             La carte
           </Link>
         </il>
-        <il className={styles.liLinkWrapper}>
-          <Link className={styles.navLink} href="devenir-franchise">
+        <il className={styles.itemLinkWrapper}>
+          <Link
+            className={
+              currentPath !== franchise
+                ? styles.navLink
+                : styles.navLinkSelected
+            }
+            href="devenir-franchise"
+          >
             devenir franchisé
           </Link>
         </il>
-        <il className={styles.liLinkWrapper}>
-          <Link className={styles.navLink} href="contact">
+        <il className={styles.itemLinkWrapper}>
+          <Link
+            className={
+              currentPath != contact ? styles.navLink : styles.navLinkSelected
+            }
+            href="contact"
+          >
             Contact
           </Link>
         </il>
